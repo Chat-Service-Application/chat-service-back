d8c8b9d4e4dac264b5ba69428e0795ca56187647
윤설이 <olerlmin1234@gmail.com>
2025-01-30 21:49:24 +0900

    initial commit

---
+++ src/main/java/com/seol/chatserviceback/ChatServiceBackApplication.java
@@ -0,0 +1,13 @@
+package com.seol.chatserviceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceBackApplication.class, args);
+    }
+
+}
